e0b67ca96955b59d9f69d7d95375f0254ec61de8
Moustapha diop <moistaphadiop57@gmail.com>
2023-08-28 12:45:49 +0000

    modification

---
+++ inscription.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <!-- Font favicon -->
+            <link rel="icon" href="images/favicon.png">
+            <!-- bootstrap5 -->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+            <script src="https://kit.fontawesome.com/3e56bd9026.js" crossorigin="anonymous"></script>
+            <!-- Font Awesome6 -->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css" integrity="sha512-SgaqKKxJDQ/tAUAAXzvxZz33rmn7leYDYfBP+YoMRSENhf3zJyx3SBASt/OfeQwBHA1nxMis7mM3EV/oYT6Fdw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <!-- button navbar -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"></script>
+            <!-- tooltip -->
+            <script>
+            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+            var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+            })
+            </script>
+            <title>Sama Adminitrat</title>
+            <link rel="stylesheet" href="inscription.css">
+    </head>
+    <body>
+    <?php
+    header("Cache-Control: no-cache, must-revalidate");
+    header("Expires: Sat, 26 Jul 2025 05:00:00 GMT");
+    // Code PHP va ici
+    $variable = "Bonjour, monde!";
+    echo $variable;
+// ?>
+         <div class="container">
+        //     <div class="row">
+        //         <div class="col-12 offset-md-2 col-md-8 contact">
+        //             <div class="row pt-md-5">
+        //                 <div class="col-12 offset-md-1 col-md-1 logo">
+        //                     <img src="logo-sama-admi" alt="">
+        //                  </div>
+                         
+        //                 <div class="col-12 offset-md-3 col-md-5 mb-5">
+        //                 <form action="#" method="POST">
+        //                     <div class="mb-3">
+        //                         <label for="exampleFormControlInput1" class="form-label">Prenom</label>
+        //                         <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Votre prenom*">
+        //                     </div>
+        //                     <div class="mb-3">
+        //                         <label for="exampleFormControlInput2" class="form-label">Nom</label>
+        //                         <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="Votre nom*">
+        //                     </div>
+        //                     <div class="mb-3">
+        //                         <label for="exampleFormControlInput3" class="form-label">Numéro téléphone</label>
+        //                         <input type="text" class="form-control" id="exampleFormControlInput3" placeholder="Votre numéro*">
+        //                     </div>
+        //                     <div class="mb-3">
+        //                         <label for="exampleFormControlInput4" class="form-label">Addresse email</label>
+        //                         <input type="email" class="form-control" id="exampleFormControlInput4" placeholder="name@example.com">
+        //                     </div>
+        //                     <label for="inputPassword5" class="form-label">Mots de passe</label>
+        //                     <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+
+        //                     <label for="inputPassword6" class="form-label">Confirmer mots de passe</label>
+        //                     <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpBlock">
+        //                     <div class="my-4 submit">
+        //                         <input type="submit" value="S'inscrire">
+                            
+        //                     </div>
+        //                     </form>
+        //                 </div>
+                        
+        //             </div>
+        //         </div>
+        //     </div>
+          </div> 
+    </body>
+</html>
